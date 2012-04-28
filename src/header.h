6c06b47eb6ab0aa25f8483a6cfb857f2027dbe9a
yafei.zhang <yafei.zhang@langtaojin.com>
2012-04-28 23:17:25 +0800

    fix timerfd_create

--- src/header.h
+++ src/header.h
@@ -128,12 +128,12 @@ inline int timerfd_create(int clockid, int flags)
   if (fd == -1)
     return -1;
 
-  if ((flags & SFD_NONBLOCK) && fcntl(fd, F_SETFL, O_NONBLOCK) == -1)
+  if ((flags & TFD_NONBLOCK) && fcntl(fd, F_SETFL, O_NONBLOCK) == -1)
   {
     safe_close(fd);
     return -1;
   }
-  if ((flags & SFD_CLOEXEC) && fcntl(fd, F_SETFD, FD_CLOEXEC) == -1)
+  if ((flags & TFD_CLOEXEC) && fcntl(fd, F_SETFD, FD_CLOEXEC) == -1)
   {
     safe_close(fd);
     return -1;
